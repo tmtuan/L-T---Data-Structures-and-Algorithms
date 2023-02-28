d028464c741f0782deb05fb81374f8b7695567e7
Tran Minh Tuan <tranminhtuan684@gmail.com>
2023-02-28 21:03:18 +0700

    Update BinarySearchTree.java

--- Tree/BinarySearchTree.java
+++ Tree/BinarySearchTree.java
@@ -1,9 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tmtuan.tree;
-
 /**
  *
  * @author tmtuan
@@ -23,6 +18,7 @@ public class BinarySearchTree {
     // assessor method
     public Node getRoot() { return root; }
     public int size() { return this.size; }
+    
     // Update methods
     public void insert(int value) {
        
@@ -52,13 +48,73 @@ public class BinarySearchTree {
             size++;
         }
     }   
-    
-    
+    /**
+     * Insert a node then balance the tree 
+     * @param value 
+     */
+    public void balanceInsert(int value) {
+       
+        Node curr = root;
+        while (curr != null) {  // Traverse tree to find a proper place for inserting value
+            if (value < curr.getValue()) 
+                if (curr.getLeftChild() == null) {
+                    curr.setLeftChild(new Node(value, curr, null, null));
+                    size++;
+                    restructure(curr.getLeftChild());
+                    break;
+                }
+                else 
+                    curr = curr.getLeftChild();
+               
+            if (value > curr.getValue()) 
+                if(curr.getRightChild() == null) {
+                    curr.setRightChild(new Node(value, curr, null, null));
+                    size++;
+                    restructure(curr.getRightChild());
+                    break;
+                }
+                else    
+                    curr = curr.getRightChild();
+        } 
+
+        if (root == null) {
+            root = new Node(value, null, null, null);
+            size++;
+        }
+    }   
+    /**
+     * Delete a node
+     * @param p 
+     */
     public void delete(Node p) {
         if (p.getLeftChild() == null && p.getRightChild() == null) {
             if(p.getParent()!= null) 
                 if(p.getParent().getLeftChild() == p) p.getParent().setLeftChild(null);
                 else p.getParent().setRightChild(null);
+            p = null;
+            size--;
+        }
+        else if (p.getLeftChild() != null) {
+            Node pred = predecessor(p);
+            swap(p, pred);
+            delete(pred);
+        }
+        else if (p.getRightChild() != null) {
+            Node succ = successor(p);
+            swap(p, succ);
+            delete(succ);
+        } 
+    }
+    /**
+     * Delete a node, then balance the tree
+     * @param p 
+     */
+    public void balancedDelete(Node p) {
+        if (p.getLeftChild() == null && p.getRightChild() == null) {
+            if(p.getParent()!= null) 
+                if(p.getParent().getLeftChild() == p) p.getParent().setLeftChild(null);
+                else p.getParent().setRightChild(null);
+            restructure(p.getParent());
             p = null;
             size--;
         }
@@ -83,7 +139,11 @@ public class BinarySearchTree {
         p.setValue(q.getValue());
         q.setValue(temp);
     }
-    
+    /**
+     * The predecessor of p which is the largest on the left of p
+     * @param p
+     * @return 
+     */
     public Node predecessor(Node p) {
         
         Node curr = p.getLeftChild();
@@ -98,6 +158,11 @@ public class BinarySearchTree {
         return result;    
     }
     
+    /**
+     * The successor of p which is the smallest on the right of p
+     * @param p
+     * @return 
+     */
     public Node successor(Node p) {
         
         Node curr = p.getRightChild();
@@ -156,7 +221,120 @@ public class BinarySearchTree {
         System.out.print(p.getValue() + " ");
         
     }
+    /**
+     * Height of a tree
+     * @param p
+     * @return 
+     */
+    public int height(Node p) {
+        if (p == null) return 0;
+        else return  1 + max(height(p.getLeftChild()),
+                             height(p.getRightChild()));
+    }
+    /**
+     * The max of a and b
+     * @param a
+     * @param b
+     * @return 
+     */
+    public int max(int a, int b) {
+        return a > b ? a : b;
+    }
+    /**
+     * Given y is parent of x and x is left child of y
+     * A right rotation at x turns x into a parent of y and right subtree of x becomes left subtree of y
+     * @param x 
+     */
+    public void rightRotation(Node x) {
+        
+        Node y = x.getParent();
+        y.setLeftChild(x.getRightChild());
+        x.setRightChild(y);
+        x.setParent(y.getParent());
+        y.setParent(x);
+    }
     
+    /**
+     * Given y is parent of x and x is right child of y
+     * A left rotation at x turns x into parent of y and left subtree of x becomes right subtree of y
+     * @param x 
+     */
+    public void leftRotation(Node x) {
+        
+        Node y = x.getParent();
+        y.setRightChild(x.getLeftChild());
+        x.setLeftChild(y);
+        x.setParent(y.getParent());
+        y.setParent(x);
+        
+    }
+    /**
+     * 
+     * @param x 
+     */
+    public void doubleRotation(Node x) {
+        
+    }
+    /**
+     * AVL tree has every node with its left sub-tree and right sub-tree different by 1 height
+     * @param p
+     * @return 
+     */
+    public boolean isAVL(Node p) {
+        Boolean avl;
+        if (p == null) 
+            avl = true;
+        else 
+            avl = Math.abs(height(p.getLeftChild()) - height(p.getRightChild())) == 1; 
+        return avl;
+    }
+    /**
+     * Calibrate to balance tree from node x
+     * @param x 
+     */
+    public void balance(Node x) {
+     
+        // if x's right child, named y, is 2 height higher than left child, then,
+        // if x's right child is right heavy or balanced, then leftRotate at y
+        if(x==null) return; 
+        else if ((height(x.getLeftChild()) - height(x.getRightChild())) < -1 ) {
+            if ((height(x.getRightChild().getLeftChild()) - height(x.getRightChild().getRightChild())) < - 1)
+                leftRotation(x.getRightChild());
+            else {
+                rightRotation(x.getRightChild().getLeftChild());
+                leftRotation(x.getRightChild());   
+            }
+        }
+        // if x's left child, name y is 2 height higher than right child, then,
+        // if x's left child is left heavy or balance, then rightRotate at y
+        else if ((height(x.getLeftChild()) - height(x.getRightChild())) > 1) {
+            if ((height(x.getLeftChild().getLeftChild()) - height(x.getLeftChild().getRightChild())) > 1) 
+                rightRotation((x.getLeftChild()));
+            else {
+                leftRotation(x.getLeftChild().getRightChild());
+                rightRotation(x.getLeftChild());
+            }
+        }
+        
+                
+    }
+    /**
+     * Restructure a tree from node x up to root to balance the whole tree
+     * @param x 
+     */
+    public void restructure(Node x) {
+        
+        if (x == null) return;
+        else {
+            balance(x);
+            balance(x.getParent());
+        }
+    }
+    
+    /**
+     * main
+     * @param args 
+     */
     public static void main(String[] args) {
         
         int[] a = {4, 6, 1, 3, 10, 2, 20};
@@ -167,6 +345,9 @@ public class BinarySearchTree {
             tree.insert(a[i]);
         }
        System.out.println("After inserted with several values, the tree has the size of " + tree.size());
+       System.out.println("Tree height = " + tree.height(tree.getRoot()));
+       System.out.println("Height of left subtree = " + tree.height(tree.getRoot().getLeftChild()));
+       System.out.println("Height of right subtree = " + tree.height(tree.getRoot().getRightChild()));
        
         System.out.println("\nTraverse the tree by preorder");
         tree.preorder(tree.getRoot());
@@ -177,11 +358,16 @@ public class BinarySearchTree {
        System.out.println("\nTraverse the tree by postorder");
        tree.postorder(tree.getRoot());
        
+       if (tree.isAVL(tree.getRoot()))
+           System.out.println("\nIs AVL");
+       else 
+           System.out.println("\nIs not AVL");
+       
        System.out.println("\nSearching 6");
        Node search = tree.search(tree.getRoot(), 6);
        if(search != null) {
            System.out.println("Found " + search.getValue());
-            tree.delete(search);
+            tree.balancedDelete(search);
             tree.inorder(tree.getRoot());
        }
        else
@@ -196,7 +382,15 @@ public class BinarySearchTree {
        else
            System.out.println(("Not found"));
            
-     
+       System.out.println("Height of left subtree = " + tree.height(tree.getRoot().getLeftChild()));
+       System.out.println("Height of right subtree = " + tree.height(tree.getRoot().getRightChild()));
+       
+       System.out.println("\nTraverse the tree by inorder");
+       tree.inorder(tree.getRoot());
+       if (tree.isAVL(tree.getRoot()))
+           System.out.println("Is AVL");
+       else 
+           System.out.println("Is not AVL");
        System.out.println();
                
     }
